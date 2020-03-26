830d4e28d4604bb341a82e1ff03ed74a8cca8d2b
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2020-03-26 17:18:26 +0100

    Added personal data to api

--- my-resume/gridsome.server.js
+++ my-resume/gridsome.server.js
@@ -6,8 +6,34 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
+
+  const addPersonalData = (store) => {
+    var personaldata = store.addCollection('PersonalData');
+
+    var birthDay = new Date(1977, 1, 21)
+    var ageDiffMs = Date.now() - birthDay.getTime()
+    var ageDate = new Date(ageDiffMs)
+    var age = Math.abs(ageDate.getUTCFullYear() - 1970);
+    var today = new Date();
+    var experienceYears =  Math.abs(today.getUTCFullYear() - 2001)
+
+    var data = {
+      firstName : 'Dave',
+      lastName : 'Woestenborghs',
+      age,
+      title : 'Senior Fullstack .NET developer',
+      description : `Senior Fullstack .NET developer with ${experienceYears} years experience. Specialized in Umbraco CMS implementations. Strong affinity with frontend technoligies`
+    }
+
+    store.addMetadata('personalData', data)
+
+
+  }
+
+
+  api.loadSource(async store => {
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
+    addPersonalData(store);
   })
 
   api.createPages(({ createPage }) => {
