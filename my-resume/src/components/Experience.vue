fca9c7363dd9cf037dd6a38e58345b0548916bba
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2020-03-27 08:41:16 +0100

    More components

---
+++ my-resume/src/components/Experience.vue
@@ -0,0 +1,173 @@
+<template>
+    <section>
+            <h2 class="section">
+              Experience
+            </h2>
+
+            <div>
+              <p class="font-bold">
+                Yokogawa (for HeadFWD)
+              </p>
+              <p class="flex justify-between">
+                <span class="italic">Senior Frontend Developer &nbsp;</span>
+                <span class="italic">July. 2019 &mdash; Present&nbsp;</span>
+              </p>
+              <p class="paragraph mt-4">
+                Yokogawa is responsible for industrial automation, test and
+                measurement solutions. They provide everything for measuring and
+                monitoring of factories. For this project, they wanted an
+                application that gives insight into all the data that is being
+                collected. Oscar was solely responsible for building and
+                implementing the frontend application that gives insight into
+                the collected data from different applications. This was created
+                in React. Another project was creating the interface of the
+                application that collected the data of different sensors and
+                measuring systems.
+              </p>
+              <p class="paragraph italic">
+                <strong>Skills</strong>: Typescript, React, HTML(5), CSS(3),
+                Javascript, Typescript, jQuery, Styled Components, SASS, Jira,
+                Git, Agile
+              </p>
+            </div>
+            <div>
+              <p class="font-bold mt-2">
+                HeadFWD
+              </p>
+              <p class="flex justify-between">
+                <span class="italic">Fullstack developer &nbsp;</span>
+                <span class="italic">Jul. 2019 &mdash; Present&nbsp;</span>
+              </p>
+              <p class="paragraph mt-4">
+                HeadFWD is a knowledge supplier in the field of web development.
+                With a team of 30 people, they help other companies realize
+                their goals on the frontend and backend. Oscar works as Frontend
+                Developer where he uses his technical knowledge to help large
+                companies and startups with their online ambitions.
+              </p>
+              <p class="paragraph italic">
+                <strong>Skills</strong>: JavaScript, ReactJS, Angular, NodeJS,
+                VueJS, Restful, Unit Testing, MongoDB, MySQL, Agile, SCRUM
+              </p>
+            </div>
+            <div>
+              <p class="font-bold mt-2">
+                MoneyMonk
+              </p>
+              <p class="flex justify-between">
+                <span class="italic">Fullstack&nbsp; (custom PHP/ React)</span>
+                <span class="italic">Sep. 2016 &mdash; Jul. 2019&nbsp;</span>
+              </p>
+              <div class="paragraph mt-4">
+                <p>
+                  At MoneyMonk, Oscar was asked to technically raise the core
+                  application to a higher level. Oscar has been jointly
+                  responsible at MoneyMonk for the development of the front end
+                  and back end of the core product and internal products. The
+                  core application is a web application. When Oscar joined
+                  MoneyMonk, they were busy transitioning from the front end to
+                  the back end. The backend application is made in PHP and part
+                  of the frontend application in React. Oscar has helped with:
+                </p>
+                <ul class="ml-4 mb-2">
+                  <li>
+                    Management of the internal operation of the system, Oscar
+                    has improved the system, is future-proof and easy to examine
+                    keep it made.
+                  </li>
+                  <li>
+                    Standardization, Oscar has provided standardization of
+                    tooling, techniques and coding guidelines at MoneyMonk.
+                  </li>
+                  <li>
+                    Migration from the frontend to React, Oscar helped with
+                    converting different modules.
+                  </li>
+                  <li>
+                    Introduction Redux, Oscar helped with the introduction and
+                    implementation of Redux as a status management tool.
+                  </li>
+                </ul>
+              </div>
+              <div class="paragraph">
+                <p>
+                  Another project that Oscar worked on at MoneyMonk is
+                  Touchpoint. This is an internal application for ticket
+                  handling. Oscar has worked on:
+                </p>
+                <ul class="ml-4 mb-2">
+                  <li>
+                    Management about setting up the internal ticket application.
+                    Oscar has had to build a system from the beginning together
+                    with the team.
+                  </li>
+                  <li>
+                    Set up application, Oscar has set up both the backend and
+                    frontend together with team.
+                  </li>
+                  <li>
+                    Inventory the requirements, Oscar has drawn up the
+                    requirements in consultation with the stakeholders.
+                  </li>
+                </ul>
+              </div>
+
+              <div class="paragraph">
+                <p>
+                  Oscar has also worked on a React Native project. His work
+                  within this project focused on React Native:
+                </p>
+                <ul class="ml-4 mb-2">
+                  <li>
+                    The mobile application was initially made native but was
+                    later partly created in React Native.
+                  </li>
+                  <li>
+                    Converting different existing native modules into React
+                    Native, Oscar has different modules that were native written
+                    converted to React Native.
+                  </li>
+                  <li>
+                    Implementation of state management tool Redux, Oscar has
+                    implemented Redux in the mobile application and this has
+                    delivered an enormous efficiency gain.
+                  </li>
+                </ul>
+              </div>
+
+              <div class="paragraph">
+                <p>
+                  Finally Oscar worked on the Servers within MoneyMonk. He has
+                  worked on:
+                </p>
+                <ul class="ml-4 mb-2">
+                  <li>
+                    Setting up servers with Docker, Oscar has set up various
+                    internal applications with Docker including a NodeJS express
+                    server, Grafana and a database backup server.
+                  </li>
+                  <li>
+                    In addition to the core application, Oscar has set up the
+                    servers and set up several smaller applications.
+                  </li>
+                </ul>
+              </div>
+            </div>
+
+            <p class="paragraph italic">
+              <strong>Skills</strong>: PHP, ReactJS, GIT, NodeJS, Docker,
+              Webpack, HTML5, CSS3, Fullstack Development, SASS, Vagrant,
+              PHPUnit, Bootstrap, Lin- ux, Apache, MySQL, Redis, Nginx
+            </p>
+          </section>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
